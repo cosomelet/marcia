971742c7567aa088c968d6e25d9e49ab2a39e50a
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-03-30 15:01:22 +0200

    database
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

---
+++ marcia/database.py
@@ -0,0 +1,23 @@
+from numpy import loadtxt
+import os
+
+
+__path__ = os.path.dirname(os.path.realpath(__file__))
+__datapath__ = os.path.join(__path__, 'Data')
+
+class Data:
+
+    def __init__(self,data:str) -> None:
+        assert data in ['CC']
+        self.__data_dic__ = {
+                            'CC': ['CC.txt','z','H','dH'],
+                            }
+        self.data = data
+        self.datafile = loadtxt(os.path.join(__datapath__, self.__data_dic__[data][0]))
+
+    def __getattr__(self,name: str):
+        if name in self.__data_dic__[self.data][1:]:
+            return self.datafile[self.__data_dic__[self.data].index(name)-1]
+        else:
+            raise AttributeError(f'{name} is not a valid attribute for {self.data}') #TODO: print available attributes
+
