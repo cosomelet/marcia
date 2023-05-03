b2b000f09f66894bdf430fec19332cbc6c61771b
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-05-03 04:30:18 +0200

    installation
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ Multi-task gAussian pRocess for CosmologIcal Applications
 [![Tests](https://github.com/antolonappan/marcia/actions/workflows/test.yml/badge.svg)](https://github.com/antolonappan/marcia/actions/workflows/test.yml)
 [![Documentation Status](https://readthedocs.org/projects/marcia/badge/?version=latest)](https://marcia.readthedocs.io/en/latest/?badge=latest)
 
+### Installation
+
+Clone the latest version of the code from github
+
+```
+pip install .
+```
+
 ### Documentation 
 
 Please visit [marcia.readthedocs.io](https://marcia.readthedocs.io/en/latest/)
--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-#Not Implemented
+from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+files = ["marcia/*"]
+setup(
+        name = 'marcia',
+        packages = ['marcia'],
+        package_data = {'marcia' : files },
+        version = 'beta-0.0.0',
+        install_requires = ['numpy','scipy','emcee','toml'],
+        description = 'Multi tasking Gaussian Process for cosmological inference',
+        author = ['Balakrishna Sandeep Haridasu','Anto Idicherian Lonappan'], 
+        author_email = ['mail@antolonappan.me','sandeep.haridasu@sissa.it'],
+        url = 'https://github.com/antolonappan/pycachera',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        )
