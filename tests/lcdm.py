ebf7f15388354c53f593338fb1b0630b6ecde9cf
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-05-07 17:20:19 +0200

    test for 3 done
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- tests/lcdm.py
+++ tests/lcdm.py
@@ -29,5 +29,49 @@ class TestLCDM(unittest.TestCase):
         assert np.allclose(round(self.theory.sound_horizon([70, 0.3]),2), 146.61)
     
     
+class TestwCDM(unittest.TestCase):
+    
+    def setUp(self):
+        self.theory = Cosmology('wCDM',['H0','Omega_m','w0'])
+        self.z_min = 0.001
+        self.z_max = 10.0
+        self.zlist = np.linspace(self.z_min,self.z_max,10)
 
+    def test_hubble_rate(self):
+        arr = np.array([  70.01685499,  126.67521252,  225.74229982,  348.19526433,
+                          488.78992978,  645.0625029 ,  815.45561061,  998.84168575,
+                          1194.34432306, 1401.25249321])
+        assert np.allclose(self.theory.hubble_rate( [70, 0.3, -1.2], self.zlist), arr)
+
+    def test_transverse_distance(self):
+        arr = np.array([   0.        , 3696.26106198, 5665.87144101, 6851.58369802,
+                           7657.7741683 , 8250.33207584, 8709.22521719, 9078.06811247,
+                           9382.87912752, 9640.24985304])
+        assert np.allclose(self.theory.transverse_distance( [70, 0.3, -1.2], self.zlist), arr)
+    
+    def test_rd(self):
+        assert np.allclose(round(self.theory.sound_horizon([70, 0.3, -1.2]),2), 146.61)
+
+class TestCPL(unittest.TestCase):
+
+    def setUp(self):
+        self.theory = Cosmology('CPL',['H0','Omega_m','w0','wa'])
+        self.z_min = 0.001
+        self.z_max = 10.0
+        self.zlist = np.linspace(self.z_min,self.z_max,10)
+    
+    def test_hubble_rate(self):
+        arr = np.array([   70.01686233,  127.89567272,  226.9968095 ,  349.25481344,
+                           489.66970756,  645.80057218,  816.08389501,  999.38406754,
+                           1194.81840896, 1401.67138477])
+        assert np.allclose(self.theory.hubble_rate( [70, 0.3, -1.2, 0.2], self.zlist), arr)
+    
+    def test_transverse_distance(self):
+        arr = np.array([   0.        , 3673.28337592, 5627.65857786, 6808.37226025,
+                           7612.65180016, 8204.35264164, 8662.81301718, 9031.41762285,
+                           9336.08855195, 9593.37122972])
+        assert np.allclose(self.theory.transverse_distance( [70, 0.3, -1.2, 0.2], self.zlist), arr)
+    
+    def test_rd(self):
+        assert np.allclose(round(self.theory.sound_horizon([70, 0.3, -1.2, 0.2]),2), 146.61)
 
