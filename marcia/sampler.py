cd38ffef911c9babee87c8395d2e4728f0ca6dfc
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-05-15 19:00:05 +0200

    convergence
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- marcia/sampler.py
+++ marcia/sampler.py
@@ -3,6 +3,14 @@ import numpy as np
 from marcia import Likelihood as lk
 import scipy.optimize as op
 from chainconsumer import ChainConsumer
+import logging
+import os
+
+logging.basicConfig(filename="convergence.log",format='%(asctime)s %(message)s',filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 
 class Sampler:
 
@@ -11,9 +19,6 @@ class Sampler:
 
         self.ndim = len(self.likelihood.priors)
         self.nwalkers = 100
-        self.nsteps = 2000
-        self.pburnin = 20. # percentage of burn-in steps to remove from each walker
-        self.nburnin =self.nsteps*(self.pburnin)/100
         self.initial_guess = initial_guess
 
     
@@ -26,20 +31,64 @@ class Sampler:
         return result.x
     
 
-    def sampler(self):
+    def sampler_pos(self):
         mle = self.MLE()
         pos = [mle+ 1e-4*np.random.randn(self.ndim) for i in range(self.nwalkers)]
-        sampler = emcee.EnsembleSampler(self.nwalkers, self.ndim, self.likelihood.logProb)
-        sampler.run_mcmc(pos, self.nsteps,progress=True)
-        return sampler
+        return pos
     
-    def get_chain(self):
-        sampler = self.sampler()
+    def sampler(self,filename):
+        backend = emcee.backends.HDFBackend(filename)
+        if os.path.isfile(filename):
+            return backend
+        else:
+
+            backend.reset(self.nwalkers, self.ndim)
+
+            sampler = emcee.EnsembleSampler(self.nwalkers, self.ndim, self.likelihood.logProb, backend=backend)  
+
+            max_n = 100000
+            index = 0
+            autocorr = np.empty(max_n)
+
+            old_tau = np.inf
+
+            for sample in sampler.sample(self.sampler_pos(), iterations=max_n, progress=True):
+                # Only check convergence every 100 steps
+                if sampler.iteration % 100:
+                    continue
+
+                # Compute the autocorrelation time so far
+                # Using tol=0 means that we'll always get an estimate even
+                # if it isn't trustworthy
+                tau = sampler.get_autocorr_time(tol=0)
+                autocorr[index] = np.mean(tau)
+                index += 1
+
+                # Check convergence
+                converged = np.all(tau * 100 < sampler.iteration)
+                converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+                logger.info(f'I: {sampler.iteration}, tau*100:{tau * 100}, Tol:{(old_tau - tau) / tau}')
+                if converged:
+                    print(f'Converged at iteration {sampler.iteration}')
+                    break
+                old_tau = tau
+            
+            return sampler
+
+# Initialize the sampler
+
+
+        # sampler = emcee.EnsembleSampler(self.nwalkers, self.ndim, self.likelihood.logProb)
+        # sampler.run_mcmc(pos, self.nsteps,progress=True)
+        # return sampler
+    
+    def get_chain(self,filename):
+        sampler = self.sampler(filename)
         samples = sampler.get_chain(discard=100, thin=15, flat=True)
         return samples
 
-    def corner_plot(self):
-        chains = self.get_chain()
+    def corner_plot(self,filename):
+        chains = self.get_chain(filename)
         c = ChainConsumer().add_chain(chains, parameters=self.likelihood.theory.labels)
         fig = c.plotter.plot(truth=list(self.MLE(False)))
         fig.set_size_inches(3 + fig.get_size_inches()) 
--- marcia/utils.py
+++ marcia/utils.py
@@ -1,10 +1,19 @@
+import numpy as np
+from numba import jit
+
 def load_data_once(func):
     data = None
-
     def wrapper(*args, **kwargs):
         nonlocal data
         if data is None:
             data = func(*args, **kwargs)
         return data
-
     return wrapper
+
+@jit(nopython=True)
+def nan_to_zero(arr):
+    result = arr.copy()
+    for i in range(result.shape[0]):
+        if np.isnan(result[i]):
+            result[i] = 0
+    return result
