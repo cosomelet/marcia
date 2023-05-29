79500a15c6645631dd7027c7dd657bf64b378cfd
Balakrishna Sandeep Haridasu <32479204+Sandeepharidasu@users.noreply.github.com>
2023-05-29 14:50:04 +0000

    GP config file

--- marcia/__init__.py
+++ marcia/__init__.py
@@ -1,6 +1,7 @@
 from .utils import *
 from .cosmology import *
 from .params import Params
+from .GPconfig import GPConfig
 from .database import Data 
 from .likelihood import Likelihood
 from .sampler import Sampler
