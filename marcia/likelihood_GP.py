f5fa447f576c93b289c3b0d67afc42ac05e9c4e9
Balakrishna Sandeep Haridasu <32479204+Sandeepharidasu@users.noreply.github.com>
2023-06-30 05:45:54 +0000

    update mean function in likelihood

--- marcia/likelihood_GP.py
+++ marcia/likelihood_GP.py
@@ -1,13 +1,14 @@
 import numpy as np
 from marcia import Data 
 from marcia import GPconfig
+from marcia import CosmoConfig
 from marcia import Cosmology as cosmo 
 from marcia import kernels as kern
 
 
 class Likelihood_GP(object):
     # This contains the generalized likelihood for the GP and MTGP
-    def __init__(self, model,parameters,data,GP_filename=None, cosmo_filename=None):
+    def __init__(self, data, GP_filename=None, cosmo_filename=None):
         
         # Read form the GP config file
         # read the config file
@@ -15,7 +16,22 @@ class Likelihood_GP(object):
             MTGP = GP_filename.GPConfig('GPconfig.ini').__dict__
         else:
             MTGP = GP_filename.GPConfig(GP_filename).__dict__
-        
+
+        # To read the cosmology file and inculde a comological mean function in the GP
+        if cosmo_filename is None:
+            cosmo = cosmo_filename.CosmoConfig('cosmology.ini').__dict__
+        else:
+            cosmo = cosmo_filename.CosmoConfig(cosmo_filename).__dict__
+
+        # To include the cosmological mean function in the GP
+        if cosmo['sample'] == False:
+            self.cosmo = cosmo['model']
+        elif cosmo['sample'] == True:
+            # Yet to define the cosmological mean function for the sampled cosmology with paramters sampled amongside the GP hyperparameters
+            pass
+        else:
+            self.mean = [0.0] * len(np.flatten(data.x.values()) )
+
         # read the models and the number of tasks
         self.models = MTGP['models']
         self.nTasks = MTGP['n_Tasks']
@@ -41,32 +57,28 @@ class Likelihood_GP(object):
         self.D_covmat = self.cov
 
         # intialize the kernals from Kernel class
-        self.GP_kernels = kern.Kernels(list(data.x.values())) 
+        self.GP_kernels = kern.Kernels(list(data.x.values()))
 
-    # def set_theta(self, theta):
-    #     # To reorganise the theta values for the GP hyperparameters in accordanace with the GPconfig file
-    #     # theta contains no self-scale: [sigma_f_1, l_1, sigma_f_2, l_2, ...] 
-    #     # or [sigma_f_1, sigma_f_2, ..., l] if self_scale is True 
-    #     # and possibly with intrinsic scatter at the end [..., sigma_int] 
+    def set_theta(self, theta):
+        # To reorganise the theta values for the GP hyperparameters in accordanace with the GPconfig file
+        # theta contains no self-scale: [sigma_f_1, l_1, sigma_f_2, l_2, ...] 
+        # or [sigma_f_1, sigma_f_2, ..., l] if self_scale is True 
+        # and possibly with intrinsic scatter at the end [..., sigma_int] 
 
-    #     if self.self_scale == False and len(theta) != 2*self.nTasks and self.scatter == False:
-    #         raise ValueError('The number of hyperparameters does not match the number of tasks')
-    #     elif self.self_scale == False and len(theta) != 2*self.nTasks+1 and self.scatter == True:
+        if self.self_scale == False and len(theta) != 2*self.nTasks and self.scatter == False:
+            raise ValueError('The number of hyperparameters does not match the number of tasks')
+        elif self.self_scale == False and len(theta) != 2*self.nTasks+1 and self.scatter == True:
 
-    #         params = []
-    #         for i in range(self.nTasks):
-    #             params.append(theta[2*i])
-    #             params.append(theta[2*i+1])
-    #         params = np.array(params)
+            params = []
+            for i in range(self.nTasks):
+                params.append(theta[2*i])
+                params.append(theta[2*i+1])
+            params = np.array(params)
 
-
-    #     return params
+        return params
 
 
     def logPrior(self, theta):
-        # if self.priors[0][0] < theta[0] < self.priors[0][1] and self.priors[1][0] < theta[1] < self.priors[1][1]:
-        # logPrior is independent of data for the most of it, unless otherwise some strange functions are defined
-        #if all((np.array(theta)-self.priors[:, 0])>0) and all((self.priors[:, 1]-np.array(theta))>0):
         if all(self.priors[i][0] < theta[i] < self.priors[i][1] for i in range(len(theta))):
             return 0.0
         return -np.inf
@@ -91,13 +103,14 @@ class Likelihood_GP(object):
         # To perform cholesky decomposition
         L = np.linalg.cholesky(Total_covmat)
         L_inv = np.linalg.inv(L)
-        y_inv = np.dot(L_inv, self.y)
+        y = self.y - self.mean
+        y_inv = np.dot(L_inv, y)
 
         # To calculate the log determinant
         log_det = 2*np.sum(np.log(np.diag(L)))
 
         # To calculate the chi2
-        chi2 = np.dot(y_inv.T, y_inv) + log_det
+        chi2 = np.dot(y_inv.T, y) + log_det
 
         return chi2
 
