f0e8adda30306cb1da0771b747bc476d0523b241
Balakrishna Sandeep Haridasu <32479204+Sandeepharidasu@users.noreply.github.com>
2023-07-01 08:39:18 +0000

    update kernels.py

--- marcia/kernels.py
+++ marcia/kernels.py
@@ -55,10 +55,12 @@ class Kernels(object):
         # print the chosen kernels
         print(f'Kernels: {self.models}')
 
-        # To define kernels in a dictionary
+        # To define kernels and data in a dictionary
         self.kernel_f = {}
+        self.data_f = {}
         for i in range(self.nTasks):
             self.kernel_f[f't_{i}'] = self.models[i]
+            self.data_f[f't_{i}'] = self.data[i]
         
         # To check if the  number of datasets is equal to the number of models
         if len(self.data) != self.nmodel:
@@ -121,14 +123,14 @@ class Kernels(object):
         if nu == 0.0: # Squared Exponential kernel
             return np.exp(- (x**2.)/(2. * l1**2.))
         elif nu == 5/2: # Matern 5/2 kernel
-            return  kernel.gMdef(tau, l1, nu) * _spkv_(tau,l1) / np.pi
+            return  kernel.gMdef(tau, l1, nu) * self._spkv_(tau,l1) / np.pi
         elif nu == 7/2: # Matern 7/2 kernel
             return kernel.gMdef(tau,l1, nu)
         else: # Generalised Matern kernel
             A, B, C = kernel.gMdef(tau,l1,nu)
-            A = A * (_spgamma_(nu/2. - 1./4.)/_spgamma_(nu/2. + 1./4.))**(1./2.) * (_spgamma_(nu + 1./2.)/_spgamma_(nu))**(1./4.)
-            B = B / _spgamma_(nu/2. - 1./4.)
-            return A**2. * B * C**(nu/2. - 1./4.) * _spkv_(tau,l1) / np.pi
+            A = A * (self._spgamma_(nu/2. - 1./4.)/self._spgamma_(nu/2. + 1./4.))**(1./2.) * (self._spgamma_(nu + 1./2.)/self._spgamma_(nu))**(1./4.)
+            B = B / self._spgamma_(nu/2. - 1./4.)
+            return A**2. * B * C**(nu/2. - 1./4.) * self._spkv_(tau,l1) / np.pi
 
         
     def gdMdef(self, tau, nu, l1):
@@ -136,9 +138,9 @@ class Kernels(object):
             Defines the derivative of the basis function for the GP model and returns a vector
             Finally I define only ine single function for the basis function"""
         B = sp.special.kv(1./4. * (5. - 2. * nu), np.sqrt(2.) * np.sqrt(nu) * np.sqrt(tau**2.) / l1)
-        C = np.sqrt(_spgamma_(1./2. + nu) / _spgamma_(nu))
+        C = np.sqrt(self._spgamma_(1./2. + nu) / self._spgamma_(nu))
         A, D = kernel.gdMdef(tau, l1, nu)
-        D = D * _spgamma_(1./4. + nu/2.) 
+        D = D * self._spgamma_(1./4. + nu/2.) 
         return A * B * C / D
     
     def gMMdef_integrand(self, params):
@@ -174,7 +176,7 @@ class Kernels(object):
         return np.nan_to_num((2.**(1.-nu)/sp.special.gamma(nu)) * ((np.sqrt(2.*nu) * np.abs(x1-x2))/l_s)**nu * sp.special.kv(nu, (np.sqrt(2.*nu) * np.abs(x1-x2))/l_s)) + np.eye(len(x1)) 
 
     
-    def kernel(self, model, params, x1, x2=None):
+    def kernel(self, model, params, x1, x2=None, mat_nu=None):
         """ 
             Defines the kernel function for the GP model and returns the covariance matrix 
             """
@@ -193,6 +195,8 @@ class Kernels(object):
             return params[0]**2. * self.matern(mat_nu, x1[:, None], x2[None, :], params[1])
         if model == 'M32': # Matern 3/2 kernel
             mat_nu = 3./2.
+            return params[0]**2. * self.matern(mat_nu, x1[:, None], x2[None, :], params[1])
+        if model == 'matern' and mat_nu is not None:
             return params[0]**2. * self.matern(mat_nu, x1[:, None], x2[None, :], params[1])
 
     def cross_kernel(self, model1, model2, params1, params2, x1, x2):
@@ -215,7 +219,7 @@ class Kernels(object):
             print('Error: Cross kernel not defined')
             sys.exit(0)
 
-    def Cov_Mat(self):
+    def Cov_Mat(self, params):
         """ 
             Defines the covariance matrix for the GP model and returns the covariance matrix 
             """
@@ -224,9 +228,9 @@ class Kernels(object):
         for i in range(self.nTasks):
             for j in range(self.nTasks):
                 if i == j:
-                    self.CovMat[f't_{i}{j}'] = self.kernel(self.kernel_f[f't_{i}'], self.params[i], self.data_f[f't_{i}'])
+                    self.CovMat[f't_{i}{j}'] = self.kernel(self.kernel_f[f't_{i}'], params[i], self.data_f[f't_{i}'])
                 elif i < j:
-                    self.CovMat[f't_{i}{j}'] = np.transpose(self.cross_kernel(self.kernel_f[f't_{i}'], self.kernel_f[f't_{j}'], self.params[i], self.params[j], self.data_f[f't_{i}'], self.data_f[f't_{j}']))
+                    self.CovMat[f't_{i}{j}'] = np.transpose(self.cross_kernel(self.kernel_f[f't_{i}'], self.kernel_f[f't_{j}'], params[i], params[j], self.data_f[f't_{i}'], self.data_f[f't_{j}']))
                 else:
                     self.CovMat[f't_{i}{j}'] = np.transpose(self.CovMat[f't_{j}{i}'])
         
