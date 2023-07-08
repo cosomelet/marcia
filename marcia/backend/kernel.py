357a392b2844fd6105cdfd3a8d34723f0a98d263
Balakrishna Sandeep Haridasu <32479204+Sandeepharidasu@users.noreply.github.com>
2023-07-08 17:16:45 +0000

    backend kernel

--- marcia/backend/kernel.py
+++ marcia/backend/kernel.py
@@ -6,15 +6,15 @@ from scipy.integrate import odeint
 # To combine the above functions into a single function, we need to use the following:
 @jit(nopython=True)
 def gMdef(tau, l1, nu):
-    if nu == 7/2:
-        result =  np.sqrt(2./5.) * 7**(3./4.) * np.exp(- np.sqrt(7.) * np.abs(tau) / l1) * (1./l1**2.)**(1./4.) * np.abs(tau) * (1. + l1 / np.sqrt(7. * tau**2.))/ l1
-    else:
-        A = (nu / (2. * np.pi * l1**2.))**(1./8.) 
-        #* (sp.special.gamma(nu/2. - 1./4.)/sp.special.gamma(nu/2. + 1./4.))**(1./2.) * (sp.special.gamma(nu + 1./2.)/sp.special.gamma(nu))**(1./4.)
-        B = 2.**(5./4. - nu/2.) 
-        # / sp.special.gamma(nu/2. - 1./4.)
-        C = (2. * nu)**(1./2.) * tau**2. / l1
-        result = A,B,C # A**2. * B * C**(nu/2. - 1./4.) * sp.special.kv(nu/2. - 1./4., C)
+    # This for example is the 7/2 case, now we have only the generalised funciton !!
+    # result =  np.sqrt(2./5.) * 7**(3./4.) * np.exp(- np.sqrt(7.) * np.abs(tau) / l1) * (1./l1**2.)**(1./4.) * np.abs(tau) * (1. + l1 / np.sqrt(7. * tau**2.))/ l1
+
+    A = (nu / (2. * np.pi * l1**2.))**(1./8.) 
+    #* (sp.special.gamma(nu/2. - 1./4.)/sp.special.gamma(nu/2. + 1./4.))**(1./2.) * (sp.special.gamma(nu + 1./2.)/sp.special.gamma(nu))**(1./4.)
+    B = 2.**(5./4. - nu/2.) 
+    # / sp.special.gamma(nu/2. - 1./4.)
+    C = (2. * nu)**(1./2.) * tau**2. / l1
+    result = A,B,C # A**2. * B * C**(nu/2. - 1./4.) * sp.special.kv(nu/2. - 1./4., C)
     return result
 
 @jit(nopython=True)
@@ -25,7 +25,5 @@ def gdMdef(tau, l1, nu):
     D = np.pi**(1./4.) * x 
     # * sp.special.gamma(1./4. + nu/2.)
     return A, D 
-
-
 
     
